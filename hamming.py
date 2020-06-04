618e844c2cb5dd22d8a1a5c52a97fbb8e9deaec7
thales307 <silva.thales@gmail.com>
2020-06-03 21:53:58 -0300

    Método para calcular a distância de Hamming entre duas cadeias de DNA.

---
+++ hamming.py
@@ -0,0 +1,4 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError('DNA strands have different length')
+    return sum(1 for i in range(len(strand_a)) if strand_a[i] != strand_b[i])
